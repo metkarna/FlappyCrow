9101420108b4c6e6dd4bb181d39ba7235b7f3ea4
unknown <metkarna@gmail.com>
2020-01-07 18:16:15 +0500

    add new sprites, create game scene, add objects: bird, trees, ground, add scripts

---
+++ Assets/Scripts/TreesSpawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreesSpawn : MonoBehaviour
+{
+    public float maxTime = 1;
+    public GameObject trees;
+    private float timer = 0;
+    public float height; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newtrees = Instantiate(trees);
+        newtrees.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+        Destroy(newtrees, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > maxTime)
+        {
+            GameObject newtrees = Instantiate(trees);
+            newtrees.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+            Destroy(newtrees, 3);
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+    }
+}
