631fc11db575bb7ad7f8d35d9083618054fd5254
unknown <metkarna@gmail.com>
2020-01-12 15:39:22 +0500

    add score, game manager, deathcam, replay btn

---
+++ Assets/Scripts/AddScore.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddScore : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        Debug.Log("+");
+        // Score.score++;
+    }
+
+}
--- Assets/Scripts/Fly.cs
+++ Assets/Scripts/Fly.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 
 public class Fly : MonoBehaviour
 {
+    public GameManager gameManager;
     public float velocity = 1;
     private Rigidbody2D rb;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
         {
-            // Jump
             rb.velocity = Vector2.up * velocity;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+           gameManager.GameOver();
     }
 }
